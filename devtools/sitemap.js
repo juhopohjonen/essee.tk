8d027960b693581824eeba3c6be763c4f6559267
Juho Pohjonen <juhopohjonen237@gmail.com>
2022-11-24 10:16:29 +0200

    added sitemap tools + sitemap

--- devtools/sitemap.js
+++ devtools/sitemap.js
@@ -1,25 +1,27 @@
 // build xml with titleRoutings
 
+const fs = require('fs')
+
 const args = process.argv.slice(2)
 const url = args[0]
 
 console.log(url)
 
 const { create } = require('xmlbuilder2')
-const titleRoutings = require('../src/titleRoutings')
+const routingObj = require('../src/titleRoutings')
+
+const titleRoutings = routingObj.titleRoutings
 
 const doc = create({ version: 1.0, encoding: "UTF-8" })
-doc.ele('urlset', { xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" }).up()
-
-const url_elem = doc.ele('url')
+const url_elem = doc.ele('urlset', { xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" }).ele('url')
 
 for (let i = 0; i < titleRoutings.length; i++) {
     let routing = titleRoutings[i]
-    console.log('looping')
-    
     url_elem.ele('loc').txt(url + routing.url)
 }
 
 
-const sitemapXML = root.end({ prettyPrint: true })
+const sitemapXML = doc.end({ prettyPrint: true })
+fs.writeFileSync('./sitemap.xml', String(sitemapXML))
+
 console.log(sitemapXML)
